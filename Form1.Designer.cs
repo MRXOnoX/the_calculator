36fce6ef71368a16ad1f5062d0c84a15a1787e5d
Luka <77533373+MRXOnoX@users.noreply.github.com>
2022-04-12 15:34:26 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LTbuttonOne = new System.Windows.Forms.Button();
+            this.LTbutton1 = new System.Windows.Forms.Button();
+            this.LTbutton2 = new System.Windows.Forms.Button();
+            this.LTbutton3 = new System.Windows.Forms.Button();
+            this.LTbutton4 = new System.Windows.Forms.Button();
+            this.LTbutton5 = new System.Windows.Forms.Button();
+            this.LTbutton6 = new System.Windows.Forms.Button();
+            this.LTbutton7 = new System.Windows.Forms.Button();
+            this.LTbutton8 = new System.Windows.Forms.Button();
+            this.LTbutton9 = new System.Windows.Forms.Button();
+            this.LTbutton11 = new System.Windows.Forms.Button();
+            this.LTbutton12 = new System.Windows.Forms.Button();
+            this.LTbutton13 = new System.Windows.Forms.Button();
+            this.LTbutton14 = new System.Windows.Forms.Button();
+            this.LTbutton15 = new System.Windows.Forms.Button();
+            this.LTbutton16 = new System.Windows.Forms.Button();
+            this.LTbutton17 = new System.Windows.Forms.Button();
+            this.LTbutton19 = new System.Windows.Forms.Button();
+            this.LTtextBox_Result = new System.Windows.Forms.TextBox();
+            this.LTlabelCurrentOperation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOne
+            // 
+            this.LTbuttonOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbuttonOne.Location = new System.Drawing.Point(22, 73);
+            this.LTbuttonOne.Name = "LTbuttonOne";
+            this.LTbuttonOne.Size = new System.Drawing.Size(45, 45);
+            this.LTbuttonOne.TabIndex = 0;
+            this.LTbuttonOne.Text = "7";
+            this.LTbuttonOne.UseVisualStyleBackColor = true;
+            this.LTbuttonOne.Click += new System.EventHandler(this.button_click);
+            // 
+            // button1
+            // 
+            this.LTbutton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton1.Location = new System.Drawing.Point(73, 73);
+            this.LTbutton1.Name = "LTbutton1";
+            this.LTbutton1.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton1.TabIndex = 1;
+            this.LTbutton1.Text = "8";
+            this.LTbutton1.UseVisualStyleBackColor = true;
+            this.LTbutton1.Click += new System.EventHandler(this.button_click);
+            // 
+            // button2
+            // 
+            this.LTbutton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton2.Location = new System.Drawing.Point(124, 73);
+            this.LTbutton2.Name = "LTbutton2";
+            this.LTbutton2.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton2.TabIndex = 2;
+            this.LTbutton2.Text = "9";
+            this.LTbutton2.UseVisualStyleBackColor = true;
+            this.LTbutton2.Click += new System.EventHandler(this.button_click);
+            // 
+            // button3
+            // 
+            this.LTbutton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton3.Location = new System.Drawing.Point(175, 73);
+            this.LTbutton3.Name = "LTbutton3";
+            this.LTbutton3.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton3.TabIndex = 3;
+            this.LTbutton3.Text = "/";
+            this.LTbutton3.UseVisualStyleBackColor = true;
+            this.LTbutton3.Click += new System.EventHandler(this.operator_click);
+            // 
+            // button4
+            // 
+            this.LTbutton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton4.Location = new System.Drawing.Point(226, 73);
+            this.LTbutton4.Name = "LTbutton4";
+            this.LTbutton4.Size = new System.Drawing.Size(53, 45);
+            this.LTbutton4.TabIndex = 4;
+            this.LTbutton4.Text = "CE";
+            this.LTbutton4.UseVisualStyleBackColor = true;
+            this.LTbutton4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.LTbutton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton5.Location = new System.Drawing.Point(226, 124);
+            this.LTbutton5.Name = "LTbutton5";
+            this.LTbutton5.Size = new System.Drawing.Size(53, 45);
+            this.LTbutton5.TabIndex = 9;
+            this.LTbutton5.Text = "C";
+            this.LTbutton5.UseVisualStyleBackColor = true;
+            this.LTbutton5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.LTbutton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton6.Location = new System.Drawing.Point(175, 124);
+            this.LTbutton6.Name = "LTbutton6";
+            this.LTbutton6.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton6.TabIndex = 8;
+            this.LTbutton6.Text = "*";
+            this.LTbutton6.UseVisualStyleBackColor = true;
+            this.LTbutton6.Click += new System.EventHandler(this.operator_click);
+            // 
+            // button7
+            // 
+            this.LTbutton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton7.Location = new System.Drawing.Point(124, 124);
+            this.LTbutton7.Name = "LTbutton7";
+            this.LTbutton7.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton7.TabIndex = 7;
+            this.LTbutton7.Text = "6";
+            this.LTbutton7.UseVisualStyleBackColor = true;
+            this.LTbutton7.Click += new System.EventHandler(this.button_click);
+            // 
+            // button8
+            // 
+            this.LTbutton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton8.Location = new System.Drawing.Point(73, 124);
+            this.LTbutton8.Name = "LTbutton8";
+            this.LTbutton8.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton8.TabIndex = 6;
+            this.LTbutton8.Text = "5";
+            this.LTbutton8.UseVisualStyleBackColor = true;
+            this.LTbutton8.Click += new System.EventHandler(this.button_click);
+            // 
+            // button9
+            // 
+            this.LTbutton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton9.Location = new System.Drawing.Point(22, 124);
+            this.LTbutton9.Name = "LTbutton9";
+            this.LTbutton9.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton9.TabIndex = 5;
+            this.LTbutton9.Text = "4";
+            this.LTbutton9.UseVisualStyleBackColor = true;
+            this.LTbutton9.Click += new System.EventHandler(this.button_click);
+            // 
+            // button11
+            // 
+            this.LTbutton11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton11.Location = new System.Drawing.Point(175, 175);
+            this.LTbutton11.Name = "LTbutton11";
+            this.LTbutton11.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton11.TabIndex = 13;
+            this.LTbutton11.Text = "-";
+            this.LTbutton11.UseVisualStyleBackColor = true;
+            this.LTbutton11.Click += new System.EventHandler(this.operator_click);
+            // 
+            // button12
+            // 
+            this.LTbutton12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton12.Location = new System.Drawing.Point(124, 175);
+            this.LTbutton12.Name = "LTbutton12";
+            this.LTbutton12.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton12.TabIndex = 12;
+            this.LTbutton12.Text = "3";
+            this.LTbutton12.UseVisualStyleBackColor = true;
+            this.LTbutton12.Click += new System.EventHandler(this.button_click);
+            // 
+            // button13
+            // 
+            this.LTbutton13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton13.Location = new System.Drawing.Point(73, 175);
+            this.LTbutton13.Name = "LTbutton13";
+            this.LTbutton13.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton13.TabIndex = 11;
+            this.LTbutton13.Text = "2";
+            this.LTbutton13.UseVisualStyleBackColor = true;
+            this.LTbutton13.Click += new System.EventHandler(this.button_click);
+            // 
+            // button14
+            // 
+            this.LTbutton14.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton14.Location = new System.Drawing.Point(22, 175);
+            this.LTbutton14.Name = "LTbutton14";
+            this.LTbutton14.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton14.TabIndex = 10;
+            this.LTbutton14.Text = "1";
+            this.LTbutton14.UseVisualStyleBackColor = true;
+            this.LTbutton14.Click += new System.EventHandler(this.button_click);
+            // 
+            // button15
+            // 
+            this.LTbutton15.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton15.Location = new System.Drawing.Point(226, 175);
+            this.LTbutton15.Name = "LTbutton15";
+            this.LTbutton15.Size = new System.Drawing.Size(53, 96);
+            this.LTbutton15.TabIndex = 19;
+            this.LTbutton15.Text = "=";
+            this.LTbutton15.UseVisualStyleBackColor = true;
+            this.LTbutton15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.LTbutton16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton16.Location = new System.Drawing.Point(175, 226);
+            this.LTbutton16.Name = "LTbutton16";
+            this.LTbutton16.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton16.TabIndex = 18;
+            this.LTbutton16.Text = "+";
+            this.LTbutton16.UseVisualStyleBackColor = true;
+            this.LTbutton16.Click += new System.EventHandler(this.operator_click);
+            // 
+            // button17
+            // 
+            this.LTbutton17.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton17.Location = new System.Drawing.Point(124, 226);
+            this.LTbutton17.Name = "LTbutton17";
+            this.LTbutton17.Size = new System.Drawing.Size(45, 45);
+            this.LTbutton17.TabIndex = 17;
+            this.LTbutton17.Text = ".";
+            this.LTbutton17.UseVisualStyleBackColor = true;
+            this.LTbutton17.Click += new System.EventHandler(this.button_click);
+            // 
+            // button19
+            // 
+            this.LTbutton19.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTbutton19.Location = new System.Drawing.Point(22, 226);
+            this.LTbutton19.Name = "LTbutton19";
+            this.LTbutton19.Size = new System.Drawing.Size(96, 45);
+            this.LTbutton19.TabIndex = 15;
+            this.LTbutton19.Text = "0";
+            this.LTbutton19.UseVisualStyleBackColor = true;
+            this.LTbutton19.Click += new System.EventHandler(this.button_click);
+            // 
+            // textBox_Result
+            // 
+            this.LTtextBox_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTtextBox_Result.Location = new System.Drawing.Point(22, 40);
+            this.LTtextBox_Result.Name = "LTtextBox_Result";
+            this.LTtextBox_Result.Size = new System.Drawing.Size(257, 29);
+            this.LTtextBox_Result.TabIndex = 20;
+            this.LTtextBox_Result.Text = "0";
+            this.LTtextBox_Result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelCurrentOperation
+            // 
+            this.LTlabelCurrentOperation.AutoSize = true;
+            this.LTlabelCurrentOperation.BackColor = System.Drawing.SystemColors.Window;
+            this.LTlabelCurrentOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LTlabelCurrentOperation.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.LTlabelCurrentOperation.Location = new System.Drawing.Point(18, 13);
+            this.LTlabelCurrentOperation.Name = "LTlabelCurrentOperation";
+            this.LTlabelCurrentOperation.Size = new System.Drawing.Size(0, 24);
+            this.LTlabelCurrentOperation.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 291);
+            this.Controls.Add(this.labelCurrentOperation);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonOne);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LTbuttonOne;
+        private System.Windows.Forms.Button LTbutton1;
+        private System.Windows.Forms.Button LTbutton2;
+        private System.Windows.Forms.Button LTbutton3;
+        private System.Windows.Forms.Button LTbutton4;
+        private System.Windows.Forms.Button LTbutton5;
+        private System.Windows.Forms.Button LTbutton6;
+        private System.Windows.Forms.Button LTbutton7;
+        private System.Windows.Forms.Button LTbutton8;
+        private System.Windows.Forms.Button LTbutton9;
+        private System.Windows.Forms.Button LTbutton11;
+        private System.Windows.Forms.Button LTbutton12;
+        private System.Windows.Forms.Button LTbutton13;
+        private System.Windows.Forms.Button LTbutton14;
+        private System.Windows.Forms.Button LTbutton15;
+        private System.Windows.Forms.Button LTbutton16;
+        private System.Windows.Forms.Button LTbutton17;
+        private System.Windows.Forms.Button LTbutton19;
+        private System.Windows.Forms.TextBox LTtextBox_Result;
+        private System.Windows.Forms.Label LTlabelCurrentOperation;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LTbutton_click(object sender, EventArgs e)
+        {
+            if ((LTtextBox_Result.Text == "0") || (isOperationPerformed))
+                LTtextBox_Result.Clear();
+
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+            if (button.Text == ".")
+            { 
+               if(!LTtextBox_Result.Text.Contains("."))
+                   LTtextBox_Result.Text = LTtextBox_Result.Text + LTbutton.Text;
+
+            }else
+            LTtextBox_Result.Text = LTtextBox_Result.Text + LTbutton.Text;
+
+
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (resultValue != 0)
+            {
+                LTbutton15.PerformClick();
+                operationPerformed = button.Text;
+                LTlabelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+            else
+            {
+
+                operationPerformed = button.Text;
+                resultValue = Double.Parse(LTtextBox_Result.Text);
+                LTlabelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+        }
+
+        private void LTbutton4_Click(object sender, EventArgs e)
+        {
+            LTtextBox_Result.Text = "0";
+        }
+
+        private void LTbutton5_Click(object sender, EventArgs e)
+        {
+            LTtextBox_Result.Text = "0";
+            resultValue = 0;
+        }
+
+        private void LTbutton15_Click(object sender, EventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    LTtextBox_Result.Text = (resultValue + Double.Parse(LTtextBox_Result.Text)).ToString();
+                    break;
+                case "-":
+                    LTtextBox_Result.Text = (resultValue - Double.Parse(LTtextBox_Result.Text)).ToString();
+                    break;
+                case "*":
+                    LTtextBox_Result.Text = (resultValue * Double.Parse(LTtextBox_Result.Text)).ToString();
+                    break;
+                case "/":
+                    LTtextBox_Result.Text = (resultValue / Double.Parse(LTtextBox_Result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            resultValue = Double.Parse(LTtextBox_Result.Text);
+            LTlabelCurrentOperation.Text = "";
+        }
+    }
+}
